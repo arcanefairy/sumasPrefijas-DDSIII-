d4ee8a0e0b9b128b961eac6c497c8f04eab33677
arcanefairy <130689169+arcanefairy@users.noreply.github.com>
2023-08-18 19:53:22 -0700

    SumasPrefijas.java

--- src/sumasprefijas/SumasPrefijas.java
+++ src/sumasprefijas/SumasPrefijas.java
@@ -1,34 +1,22 @@
-package sumasprefijas;
-
- // @author Adriana Leon
-
-
- 
-public class SumasPrefijas {
-
- 
-
-    
+public class sumasprefijas {
     public static void main(String[] args) {
-       if (args.length == 0) {
-            System.out.println("Se debe proporcionar al menos un número como argumento.");
+        if (args.length == 0) {
+            System.out.println("Debe proporcionar al menos un número como argumento.");
             return;
-        } //Este if es solo un pequeño recordatorio que el usuario debe insertar mínimo un número para que el programa
-       // funcione
+        }
 
         int[] numeros = new int[args.length];
-        int[] SumP = new int[args.length]; //
-        
+        int[] SumP = new int[args.length];
 
+        // Recopilación de números e cálculo de la suma prefija
         for (int i = 0; i < args.length; i++) {
             numeros[i] = Integer.parseInt(args[i]);
             SumP[i] = (i == 0) ? numeros[i] : SumP[i - 1] + numeros[i];
         }
-
+        
+        // Impresión de números y suma prefija
         for (int i = 0; i < args.length; i++) {
             System.out.println(numeros[i] + ", " + SumP[i]);
         }
     }
-    }
-       
-
+}
